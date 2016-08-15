6d57861088b0a58a1b933ba4ffe765cef184d84b
Anwar S. Haidar <anwar.haidar@gmail.com>
2016-08-14 22:45:56 -0400

    Initial commit

---
+++ src/main/scala/recfun/Main.scala
@@ -0,0 +1,52 @@
+package recfun
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+
+  def pascal(c: Int, r: Int): Int =
+    if (c == 0 || c == r) 1 else pascal(c - 1, r - 1) + pascal(c, r - 1)
+
+  /**
+   * Exercise 2
+   */
+
+  def balance(chars: List[Char]): Boolean = {
+    var leftBrace = 0
+    var rightBrace = 0
+
+    def iterBalance(ch: List[Char]): Boolean = {
+
+      if (!ch.isEmpty) {
+        if (ch.head == '(') leftBrace += 1
+        if (ch.head == ')') rightBrace += 1
+        if (rightBrace <= leftBrace) iterBalance(ch.tail) else false
+      } else
+        leftBrace == rightBrace
+    }
+
+    iterBalance(chars)
+  }
+
+  /**
+   * Exercise 3
+   */
+    def countChange(money: Int, coins: List[Int]): Int = {
+      if(money == 0)
+        1
+      else if(money > 0 && !coins.isEmpty)
+        countChange(money - coins.head, coins) + countChange(money, coins.tail)
+      else
+        0
+    }
+  }
---
+++ src/main/scala/recfun/test.sc
@@ -0,0 +1,15 @@
+object rationals {
+
+class Rational(x: Int, y: Int) {
+  def numer = x
+  def denom = y
+}
+  val x = new Rational(1,2)
+  x.numer
+  x.denom
+
+  2.0*3 / 4
+  5+3*2+1
+5 + 3 * 4 * 2
+
+}
